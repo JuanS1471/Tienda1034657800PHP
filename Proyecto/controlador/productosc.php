333464ec9f55fc39c93ecd3ef9790c19b045caa4
JuanS1471 <86208230+JuanS1471@users.noreply.github.com>
2021-11-26 18:51:28 -0500

    .
    .

--- Proyecto/controlador/productosc.php
+++ Proyecto/controlador/productosc.php
@@ -1,137 +1,90 @@
-
-
 <?php
-
-require_once '../modelo/productosM.php';
 require_once './Mensajes.php';
+require_once '../modelo/productosM.php';
 
 class ProductosC extends Productos {
 
     function __construct() {
         switch ($_REQUEST['accion']) {
-            case "Guardar":
-                $this->head();
-                $this->resgistrar();
-                $this->footer();
-                break;
-            case "Actualizar":
-                $this->head();
-                $this->actualizar();
-                $this->footer();
-                break;
-            case "Eliminar":
-                $this->head();
-                $this->eliminar();
-                $this->footer();
-                break;
             case "Consultar":
-                $this->consultar();
+                $this->consultarPorCB();
                 break;
-            default:
+            case "ConsultarImg":
+                $this->consultarImagenes();
                 break;
         }
     }
 
-    function head() {
-        echo '<!DOCTYPE html>
-        <html>
-        <head>
-            <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-            <script src="../bootstrap/js/bootstrap.bundle.js" type="text/javascript"></script>
-            <script src="../bootstrap/js/popper.min.js" type="text/javascript"></script>
-            <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        </head>
-        <body>';
-    }
-
-    function footer() {
-        echo '</body>
-        </html>';
-    }
-
-    function consultar() {
-        if ($this->validarDatos()) {
-            $sql = "SELECT * FROM Productos  WHERE cb='" . $this->getCb() . "'";
-            $this->ejecutarSelect($sql);
-        }
-    }
-
-    function eliminar() {
-        if ($this->validarDatos()) {
-            $sql = "DELETE FROM Productos  WHERE cb='" . $this->getCb() . "'";
-            $this->ejecutarQuery($sql, "El producto se ha eliminado en el base.");
-        } else {
-            Mensajes::info("Faltan datos, no se pudo registrar");
-        }
-    }
-
-    function resgistrar() {
-        if ($this->validarDatos()) {
-            $sql = "INSERT INTO Personas VALUES('" . $this->getCb() . "','" . $this->getNombre() . "','" . $this->getDescripcion() . "')";
-            $this->ejecutarQuery($sql, "El producto se ha registrado en el base.");
-        } else {
-            Mensajes::info("Faltan datos, no se pudo registrar");
-        }
-    }
-
-    function actualizar() {
-        if ($this->validarDatos()) {
-            $sql = "UPDATE Personas SET  cb='" . $this->getCb() . "' , nombre='" . $this->getNombre() . "', descripcion='" . $this->getDescripcion() . "' WHERE cb='" . $this->getCb() . "'";
-            $this->ejecutarQuery($sql, "El producto se ha actualizado en el base.");
-        } else {
-            Mensajes::info('Los datos no se pueden actualizar.');
-        }
-    }
-
-    function ejecutarSelect($sql) {
-        require_once 'conexion.php';
+    function consultarPorCB() {
+        require_once '../controlador/conexion.php';
+        $sql = "SELECT * FROM Productos where cb='" . $_POST['cb'] . "'";
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
+
+            if ($row = $result->fetch_assoc()) {
                 $p = array($row['cb'], $row['nombre'], $row['descripcion']);
-
                 $myJSON = json_encode($p);
-
-                echo $myJSON;//[1013,Juan,Milena,Herrera,García,jua@h.com]
+                echo $myJSON;
             }
         } else {
-            Mensajes::info("No hay resultados");
+            Mensajes::info("El producto no se encuentra registrado.");
         }
         $conn->close();
     }
 
-    function ejecutarQuery($sql, $msj) {
-        require_once 'conexion.php';
-        if ($conn->query($sql) === TRUE) {
-            Mensajes::success($msj);
-        } else {
-            Mensajes::danger($conn->error);
-        }
+    function consultarImagenes() {
+        require_once '../controlador/conexion.php';
+        $sql = "SELECT * FROM Fotos where productoscb='" . $_POST['cb'] . "'";
+        $result = $conn->query($sql);
 
+        if ($result->num_rows > 0) {
+            ?>
+            <!-- Carousel -->
+            <div class="m-0 row justify-content-center">
+                <div id="demo" class="carousel slide" data-bs-ride="carousel" style="width: 400px">
+
+
+
+                    <!-- The slideshow/carousel -->
+                    <div class="carousel-inner">
+                        <?php
+                        $i = 0;
+                        while ($row = $result->fetch_assoc()) {
+                            $a = $i == 0 ? "active" : "";
+                            echo '<div class="carousel-item ' . $a . '">';
+                            echo '<img src="img/' . $row['foto'] . '" alt="' . $row['descripcion'] . '" class="d-block w-100" style="width:300px">';
+                            echo '</div>';
+                            $i++;
+                        }
+                        ?>
+                    </div>
+                    <!-- Indicators/dots -->
+                    <div class="carousel-indicators">
+                        <?php
+                        for ($j = 0; $j < $i; $j++) {
+                            $a = $j == 0 ? "active" : "";
+                            echo '<button type="button" data-bs-target="#demo" data-bs-slide-to="' . $j . '" class="' . $a . '"></button>';
+                        }
+                        ?>
+                    </div>
+                    <!-- Left and right controls/icons -->
+                    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </button>
+                </div>
+            </div>
+
+            <?php
+        } else {
+            Mensajes::info("El producto no tiene imágenes");
+        }
         $conn->close();
-    }
-
-    function validarDatos() {
-        if (isset($_POST['Documento'])) {
-
-            $this->setCb($_POST['cb']);
-            if ($_POST['accion'] != "Consultar") {
-                $this->setCb($_POST['cb']);
-                $this->setNombre($_POST['nombre']);
-                $this->setDescripcion($_POST['descripcion']);
-            }
-
-            return true;
-        } else {
-            return false;
-        }
     }
 
 }
 
 new ProductosC();
-?>
-
-
--- Proyecto/productos.php
+++ Proyecto/productos.php
@@ -1,121 +1,75 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8"/>
-        <title>Productos</title>
-
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="bootstrap/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-        <script src="bootstrap/js/popper.min.js" type="text/javascript"></script>
-        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script type="text/javascript">
+            function enviar(accion) {
+                var form = document.productosF;
+                form.action = "controlador/productosC.php?accion=" + accion;
+                //form.submit();
+                ajax();
+
+            }
             function ajax() {
-                var d = document.getElementById("cb").value;
+                var cb = document.getElementById("cb").value;
                 const xmlhttp = new XMLHttpRequest();
                 xmlhttp.onload = function () {
                     try {
-                        myObj = JSON.parse(this.responseText);//[1013,Juan,Milena,Herrera,García,jua@h.com]
-                        document.getElementById("cb").value = myObj[1];
-                        document.getElementById("nombre").value = myObj[2];
-                        document.getElementById("descripcion").value = myObj[3];
+                        myObj = JSON.parse(this.responseText);
+                        document.getElementById("nombre").value = myObj[1];
+                        document.getElementById("descripcion").value = myObj[2];
+                        ajaxImg(cb);
                     } catch (e) {
-                        document.getElementById("cb").value = "";
                         document.getElementById("nombre").value = "";
                         document.getElementById("descripcion").value = "";
-                        document.getElementById("ver").innerHTML = this.responseText;
+                        document.getElementById("imgs").innerHTML = this.responseText;
                     }
 
                 }
-                xmlhttp.open("POST", "controlador/productosc.php");
+                xmlhttp.open("POST", "controlador/productosC.php");
                 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xmlhttp.send("accion=Consultar&Documento=" + d);
-
+                xmlhttp.send("accion=Consultar&cb=" + cb);
             }
-
-
-
-            function enviarF(accion) {
-                var form = document.enviar;
-                form.action = "controlador/productosc.php?accion=" + accion;
-                var d = document.getElementById("Documento");
-                var pn = document.getElementById("pnombre");
-                var pa = document.getElementById("papellido");
-                var sub = true;
-                if (accion == "Guardar") {
-                    if (d.value == "") {
-                        sub = false;
-                        d.style = "border-color:red;";
-                    }
-                    if (pn.value == "") {
-                        sub = false;
-                        pn.style = "border-color:red;";
-                    }
-                    if (pa.value == "") {
-
-                        sub = false;
-                        pa.style = "border-color:red;";
-                    }
+            function ajaxImg(cb) {
+                const xmlhttp = new XMLHttpRequest();
+                xmlhttp.onload = function () {
+                    document.getElementById("imgs").innerHTML = this.responseText;
                 }
-                if (accion == "Eliminar") {
-                    if (d.value == "") {
-                        sub = false;
-                        d.style = "border-color:red;";
-                    }
-                }
-                if (accion == "Consultar") {
-                    sub = false;
-                    if (d.value == "") {
-                        d.style = "border-color:red;";
-                    } else {
-                        ajax();
-                    }
-                }
-             if (sub) {
-                    form.submit();
-                }
+                xmlhttp.open("POST", "controlador/productosC.php");
+                xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xmlhttp.send("accion=ConsultarImg&cb=" + cb);
             }
         </script>
-        <style type="text/css">
-            input{
-                padding: 5px;
-                text-align: center;
-                font-family: cursive;
-            }
-            #tabla1{
-                width: 434px;
-
-            }
-        </style>
     </head>
     <body>
-
-
-        <form action="" method="post" name="enviar">
-            <table id="tabla1" class="table table-hover table-bordered">
+        <form name="productosF" method="post">
+            <table class="table table-bordered table-striped">
                 <tr>
-                    <td><label for="cb" class="form-label">Codigo De Barras</label></td>
-                    <td><input type="text" class="form-control" id="cb" name="cb" ></td>
+                    <td><label for="cb">Código de barras</label></td>
+                    <td><input id="cb" class="form-control" name="cb" type="text"/></td>
                 </tr>
                 <tr>
-                    <td><label for="nombre" class="form-label">Nombre</label></td>
-                    <td><input type="text" class="form-control" id="nombre" name="nombre"></td>
+                    <td><label for="nombre">Nombre</label></td>
+                    <td><input id="nombre" class="form-control" name="nombre" type="text"/></td>
                 </tr>
                 <tr>
-                    <td><label for="descripcion" class="form-label">Descripcion</label></td>
-                    <td><input type="text" class="form-control" id="descripcion" name="descripcion"></td>
+                    <td><label for="descripcion">Descripción</label></td>
+                    <td><textarea id="descripcion" class="form-control" name="descripcion"></textarea></td>
                 </tr>
                 <tr>
-                    <td><input type="button" value="Guardar" class="btn btn-success"  onclick="enviarF('Guardar')"></td>
-                    <td><input type="button" value="Actualizar" class="btn btn-dark" onclick="enviarF('Actualizar')"></td>
-                </tr>
-                <tr>
-                    <td><input type="button" value="Eliminar" class="btn btn-danger" onclick="enviarF('Eliminar')"></td>
-                    <td><input type="button" value="Consultar" class="btn btn-info" onclick="enviarF('Consultar')"></td>
+                    <td></td>
+                    <td><input id="consultar" class="btn btn-info" name="accion" type="button" value="Consultar" onclick="ajax()"/></td>
                 </tr>
             </table>
+            <div id="imgs">
+
+            </div>
+
         </form>
-        <div id="ver">
-        </div>
-        <?php ?>
+
+        <hr/>
+        
+           
     </body>
 </html>
